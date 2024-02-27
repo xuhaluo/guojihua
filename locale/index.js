93c0d10aab7d0e7055941c2ebd50c6d2350cfd5e
马浩 <1920199810@qq.com>
2024-02-27 16:30:31 +0800

    提交代码

---
+++ locale/index.js
@@ -0,0 +1,12 @@
+import en from './en.json'
+import zhHans from './zh-Hans.json'
+import zhHant from './zh-Hant.json'
+import ja from './ja.json'
+import koKr from './ko-Kr.json'
+export default {
+	en,
+	'zh-Hans': zhHans,
+	'zh-Hant': zhHant,
+	ja,
+	'ko-Kr':koKr
+}
